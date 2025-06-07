99c5f965d9071d177d2f6492dab492efc2a713f3
Sam <exaeous@gmail.com>
2025-06-07 20:35:47 +0530

    Create the grids using only JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const container = document.querySelector(".container");
+
+for (let i = 0; i <= 16; i++) {
+  const grids = document.createElement("div");
+  grids.classList.add("grids");
+  container.appendChild(grids);
+}
