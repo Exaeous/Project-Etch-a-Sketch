c47eef85240ad342d4e14c046dc1958398425bd1
Sam <exaeous@gmail.com>
2025-06-08 21:07:17 +0530

    Add hover EventListener to change color of divs

--- script.js
+++ script.js
@@ -2,8 +2,17 @@
 
 const container = document.querySelector(".container");
 
+// Creating the grids
 for (let i = 1; i <= 16; i++) {
-  const grids = document.createElement("div");
+  let grids = document.createElement("div");
   grids.classList.add("grids");
   container.appendChild(grids);
+
+  // Changing color on mouse hover
+  grids = document.querySelectorAll(".grids");
+  grids.forEach((grid) => {
+    grid.addEventListener("mouseover", () => {
+      grid.style.backgroundColor = "black";
+    });
+  });
 }
