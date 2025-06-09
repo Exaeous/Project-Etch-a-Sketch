b70c5a9118334bfed3f12db3f692f1739fac7fb3
Sam <exaeous@gmail.com>
2025-06-09 17:07:33 +0530

    Write logic to generate the grid using only JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const container = document.querySelector(".container");
+
+function createGrid(squaresPerSide) {
+  const totalSize = 600;
+  const squareSize = totalSize / squaresPerSide;
+
+  for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
+    const square = document.createElement("div");
+    square.classList.add("grid-square");
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
+    container.appendChild(square);
+  }
+}
+
+createGrid(16);
